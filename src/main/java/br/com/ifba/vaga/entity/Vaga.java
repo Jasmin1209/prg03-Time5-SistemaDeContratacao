85584a64b4b7366a2d24048c1a3117c3736863a7
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-12-19 19:22:45 -0300

    Relacionando as classes candidatura e perfil

--- src/main/java/br/com/ifba/vaga/entity/Vaga.java
+++ src/main/java/br/com/ifba/vaga/entity/Vaga.java
@@ -5,13 +5,15 @@
 package br.com.ifba.vaga.entity;
 
 
+
+import br.com.ifba.candidatura.entity.Candidatura;
+import br.com.ifba.endereco.Endereco;
 import br.com.ifba.infrastructure.entity.PersistenceEntity;
 import br.com.ifba.vaga.enums.TipoContratacao;
 import br.com.ifba.vaga.enums.PeriodoContratacao;
 import br.com.ifba.vaga.enums.ModeloContratacao;
 import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,48 +33,56 @@ public class Vaga extends PersistenceEntity implements Serializable{
     
     
     //Título da vaga, obrigaatório e com maximo de 120 caracteres
-    @Column(nullable = false, length = 120)
+    @Column(name = "titulo", nullable = false, length = 120)
     private String titulo;
     
     ///Quantidade de vaagas disponíveis
-    @Column(nullable = false)
+    @Column(name = "quantidade", nullable = false)
     private int quantidade;
-    
+   
     // Modelo de contratação (Presencial, Híbrido, Remoto)
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "modelo_contratacao",nullable = false)
     private ModeloContratacao modelo;
     
     // Tipo de contratação (CLT, Estágio, Freelancer)
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "tipo_contratacao",nullable = false)
     private TipoContratacao tipo;
     
     // Período da contratação
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "periodo_contratacao",nullable = false)
     private PeriodoContratacao periodo;
 
     // Faixa salarial (opcional)
+    @Column(name = "faixa_salarial")
     private Long faixaSalarial;
 
     // Lista de requisitos
     @ElementCollection
+    @Column(name = "requisito")
     private Set<String> requisitos;
 
     // Lista de benefícios
     @ElementCollection
+    @Column(name = "beneficio")
     private Set<String> beneficios;
 
     // Localização da vaga
-    @Column(nullable = false)
-    private String localizacao;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "endereco_id")
+    private Endereco localizacao;
     
     // Descrição da vaga
-    @Column(length = 1000)
+    @Column(name = "descrição", length = 1000)
     private String descricao;
+    
+    @OneToMany(mappedBy = "vaga", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Candidatura> candidaturas;
+
 
     // Status da vaga (true = ativa | false = encerrada)
-    @Column(nullable = false)
+    @Column(name = "status",nullable = false)
     private Boolean status;
 }
